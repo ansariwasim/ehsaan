36947ba21e1a660ff761f520ea054f5a6d84eda3
ansariwasim <oasimansari0786gmail.com>
2024-01-02 22:01:46 +0545

    navbar check

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -28,8 +28,8 @@ function Navbar() {
      
        </div>
 
-       <div className={!nav? 'fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-500':"fixed left-[-100%]"} >
-    <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4">JENI.</h1>
+       <div className={!nav? 'fixed left-0 top-0 w-[60%] h-full border-r-gray-900 bg-[#000300] ease-in-out duration-1000':"fixed left-[-100%] "} >
+    <h1 className="w-full text-3xl font-bold text-[#00df9a] m-4 px-3">JENI.</h1>
         
 
         <ul className=' uppercase p-4'>
